4c54462cc1a3ea780d5c3ee0a6833cc307e329dd
JasonHuang <yshuang@sysnew.com>
2017-03-31 16:04:48 +0800

    person:huangyongsheng content:love milk first

---
+++ milks_assistant/milks_assistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  milks_assistant
+//
+//  Created by JasonHuang on 17/3/31.
+//  Copyright © 2017年 JasonHuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
